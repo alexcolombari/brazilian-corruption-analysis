8672df862208c92157b2b42efac18b6f9c5a6515
Alex Colombari <38837753+alexcolombari@users.noreply.github.com>
2019-05-23 17:16:52 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,33 @@
-from __future__ import absolute_import, division, print_function
-
-import pathlib
-
 import pandas as pd
+import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
 
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras import layers
-
 file_name = 'dataset.xlsx'
 df = pd.read_excel(file_name, sheet_name = 1)
-
-'''dfs = {sheet_name: xl_file.parse(sheet_name) 
-          for sheet_name in xl_file.sheet_names}'''
 
 # Drop columns with NaN values
 df = df.drop(df.columns[[3, 4, 5, 6]], axis=1)
 
 # Filter DF data with only target name
-filtered_data = df[df["Target"] == 'DEM']
-total = df['Type'].sum()
-print("Total: {}".format(total))
+empresa = 'ANDRADE GUTIERREZ'
+partido = 'PR'
+filtered = df [df["Source"] == empresa]
+filtered_data = filtered [filtered["Target"] == partido]
 
+valores = filtered_data["Value"].values
+soma = [len(valores)]
+soma = np.append(soma, valores) 
+
+if len(soma) >= 10:
+	x = "bilhões"
+if len(soma) <= 9:
+	x = "milhões"	# 1 000 000 000
+else:
+	x = "mil"
+
+#print("Total: R$ {}".format(soma.sum()))
+print("A empresa {} enviou um total de: R$ {} {} para o partido: {}"\
+	.format(empresa,soma.sum(), x, partido))
+'''total = df['Type'].max()
+print("Total: {}".format(total))'''
